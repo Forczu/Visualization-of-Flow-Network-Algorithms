f49c8afc97a99e57ec98104f86b1c22f6b755900
Forczu <mateusz.forczmanski@gmail.com>
2015-12-09 06:35:42 -0800

    CLEAR THIS SHIT

--- MainWindow/mainwindow.cpp
+++ MainWindow/mainwindow.cpp
@@ -273,6 +273,7 @@ void MainWindow::runAlgorithm(QListWidgetItem * item)
 		// zwolnienie okna z pamiêci
 		delete windowPtr;
 		// przywrócenie pierwotnego grafu
+		_graphTabs->currentGraphView()->scene()->clear();
 		auto copy = ser.deserialize(fileName);
 		_graphTabs->currentGraphView()->setGraphImage(copy);
 		copy->updateScale(_graphTabs->currentGraphView()->getScale());
